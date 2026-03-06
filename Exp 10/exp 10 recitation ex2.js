8aae868645aedb603ae4b34ac2fe94d4f0140e48
niharn23122006-sys <niharn23122006@student.sfit.ac.in>
2026-03-06 20:41:50 +0530

    Add files via upload

---
+++ Exp 10/exp 10 recitation ex2.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("Home Page");
+});
+
+app.get('/contact', (req, res) => {
+  res.send("Contact Page");
+});
+
+app.get('/help', (req, res) => {
+  res.send("Help Page");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("Home Page");
+});
+
+app.get('/contact', (req, res) => {
+  res.send("Contact Page");
+});
+
+app.get('/help', (req, res) => {
+  res.send("Help Page");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
